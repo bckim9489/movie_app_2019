ecab9d3a5b167350bd8c86162a8a8f957cab62de
bckim9489 <bckim9489@gmail.com>
2020-05-31 16:54:25 +0900

    Updates

---
+++ precache-manifest.e750016f8072b8b4b432cda23d7f3c9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef77acb5efb0c25b244dc8aa6fcaa975",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "931559a27dcb1b1d2922",
+    "url": "/movie_app_2019/static/css/main.7bf4c6c8.chunk.css"
+  },
+  {
+    "revision": "7676ec8dfa3a757764b3",
+    "url": "/movie_app_2019/static/js/2.cba50508.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2019/static/js/2.cba50508.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "931559a27dcb1b1d2922",
+    "url": "/movie_app_2019/static/js/main.feeca2d6.chunk.js"
+  },
+  {
+    "revision": "f5b00c7a31981ed629ee",
+    "url": "/movie_app_2019/static/js/runtime-main.140b3552.js"
+  }
+]);
